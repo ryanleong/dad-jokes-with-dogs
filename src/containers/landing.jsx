66f7cefdac17935923a2dfe95ffce8c202d0a22f
Ryan Leong <hello@ryanleong.net>
2018-06-06 20:47:34 +0800

    Create click to refresh

--- src/containers/landing.jsx
+++ src/containers/landing.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Joke from '../components/joke';
 // import Shibe from '../components/shibe';
 import Corgi from '../components/corgi';
 
+import { fetchShibe, fetchJoke } from '../actions';
+
 class Landing extends Component {
+    reload() {
+        this.props.fetchJoke();
+        this.props.fetchShibe();
+    }
+
     render() {
         return (
-            <div className="content">
+            <div className="content" onClick={() => this.reload() }>
                 <Corgi />
                 <Joke />
             </div>
@@ -15,4 +23,10 @@ class Landing extends Component {
     }
 }
 
-export default Landing;
+function mapStateToProps(state) {
+    return { 
+        joke: state.joke, 
+        image: state.image
+    };
+}
+export default connect(mapStateToProps, { fetchJoke, fetchShibe })(Landing);
